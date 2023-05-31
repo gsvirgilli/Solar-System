1f722e28fb41b22bc95aaf6fcce41ebfd6279302
Guilherme Silva Virgilli <gsvirgilli@gmail.com>
2023-05-31 15:28:50 -0300

    1° requisito

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import Header from './components/Header';
+
 function App() {
-  return (<p>Sistema Solar</p>);
+  return (
+    <Header />
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <h1>Sistema Solar</h1>
+    </header>
   );
 }
 
