ab069159d42722f6a56e80b1ae10c1ea84195fae
Guilherme Silva Virgilli <gsvirgilli@gmail.com>
2023-05-31 17:42:53 -0300

    Requisitos bonus

---
+++ src/components/MissionCard.tsx
@@ -0,0 +1,17 @@
+type MissionProps = {
+  name:string,
+  year:string,
+  country:string,
+  destination:string,
+};
+
+export default function MissionCard({ name, year, country, destination }:MissionProps) {
+  return (
+    <div data-testid="mission-card">
+      <p data-testid="mission-name">{name}</p>
+      <p data-testid="mission-year">{year}</p>
+      <p data-testid="mission-country">{country}</p>
+      <p data-testid="mission-destination">{destination}</p>
+    </div>
+  );
+}
